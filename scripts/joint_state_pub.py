4d8c4d69249a1a86d637b791e07d68ebbd4d3a54
shinfang-2f <shinfang-2f@shinfang2f-white.localdomain>
2025-02-25 17:25:17 +0800

    fixup! fixup! Add emily as articulation

--- scripts/joint_state_pub.py
+++ scripts/joint_state_pub.py
@@ -6,7 +6,7 @@ class JointStatePublisher(Node):
     def __init__(self):
         super().__init__('joint_state_publisher')
         # Create a publisher for JointState messages on the 'joint_states' topic
-        self.publisher_ = self.create_publisher(JointState, 'joint_command', 10)
+        self.publisher_ = self.create_publisher(JointState, 'isaac_joint_commands', 10)
         # Create a timer that triggers the callback every 1.0 second
         self.timer = self.create_timer(1.0, self.timer_callback)
         self.get_logger().info('JointStatePublisher node started.')
@@ -16,8 +16,8 @@ class JointStatePublisher(Node):
         # Set header with current time
         msg.header.stamp = self.get_clock().now().to_msg()
         # Define joint names and dummy positions for example purposes
-        msg.name = ['panda_joint1', 'panda_joint2', 'panda_joint3', 'panda_joint4', 'panda_joint5', 'panda_joint6', 'panda_finger_joint1', 'panda_finger_joint2']
-        msg.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        msg.name = ['panda_joint1', 'panda_joint2', 'panda_joint3', 'panda_joint4', 'panda_joint5', 'panda_joint6', 'panda_joint7','panda_finger_joint1', 'panda_finger_joint2']
+        msg.position = [0.05, -0.3, 0.0, -1.3, 0.0, 0.5, 0.05, 0.0, 0.0]
         # Optionally, you can include velocity and effort if needed
         msg.velocity = []
         msg.effort = []
